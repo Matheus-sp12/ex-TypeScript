b43c65e5a338eacde21ebb3c1078b954df7af1ef
Vinicius Matheus <viniciusomagnifico@hotmail.com>
2024-10-31 12:34:27 -0300

    first commit

---
+++ ex.js
@@ -0,0 +1,9 @@
+"use strict";
+function multi(number1, number2) {
+    return number1 * number2;
+}
+function saudacao(name) {
+    console.log(`Olá ${name}`);
+}
+saudacao("Vinicius");
+multi(6, 5);
---
+++ ex.ts
@@ -0,0 +1,10 @@
+function multi(number1: number, number2: number): number {
+    return number1 * number2;
+}
+
+function saudacao(name: string) {
+    return "Olá" + name;
+}
+
+console.log(saudacao("Vinicius"));
+console.log(multi(6,5));
